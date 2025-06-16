4bafa2ab067e3c13c106719d228c8953c0544e18
eckson888 <madudziak121@gmail.com>
2025-06-17 00:18:57 +0200

    final ui

--- my-app/src/routes/chat/MessageDisplay.svelte
+++ my-app/src/routes/chat/MessageDisplay.svelte
@@ -7,8 +7,8 @@
 <div class="flex flex-col {message.sender === 'self' ? 'items-end' : 'items-start'}">
     <div class="{message.sender === 'self' ? 'bg-primary-900 text-white' : 'bg-background text-white border-2 border-secondary-900'} rounded-lg px-4 py-2 max-w-xs sm:max-w-md break-words">
         {message.text}
+    </div>
         <div class="text-xs text-gray-200 mt-1 text-right">
             {formatTime(message.timestamp)}
         </div>
-    </div>
 </div>
